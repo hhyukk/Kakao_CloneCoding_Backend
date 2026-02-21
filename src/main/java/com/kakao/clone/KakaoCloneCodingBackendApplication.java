b47acf42a4773cb3f046f495ffacdbdb9eccf570
hhyukk <hhyukk1273@gmail.com>
2026-02-22 02:39:14 +0900

    프로젝트 생성

---
+++ src/main/java/com/kakao/clone/KakaoCloneCodingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kakao.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoCloneCodingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoCloneCodingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kakao/clone/KakaoCloneCodingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakao.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoCloneCodingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
